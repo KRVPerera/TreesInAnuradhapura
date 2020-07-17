1b2234236548a10f5f7389a685afc4a6e0db3b9a
KRVPerera <rukshan.viduranga@gmail.com>
2020-07-17 11:07:09 +0530

    Updates

---
+++ precache-manifest.7693b30f1effb65c6c89cf9e09f4c953.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d369e6d1dd64ef2d3ada7a796eaeb55",
+    "url": "/TreesInAnuradhapura/index.html"
+  },
+  {
+    "revision": "5562c74d2b6d318cf674",
+    "url": "/TreesInAnuradhapura/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "17463cc661c3eeb4fe9c",
+    "url": "/TreesInAnuradhapura/static/js/2.b8bdd308.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TreesInAnuradhapura/static/js/2.b8bdd308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5562c74d2b6d318cf674",
+    "url": "/TreesInAnuradhapura/static/js/main.c194cc02.chunk.js"
+  },
+  {
+    "revision": "24ad076a5147d4a58f36",
+    "url": "/TreesInAnuradhapura/static/js/runtime-main.052613d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TreesInAnuradhapura/static/media/logo.5d5d9eef.svg"
+  }
+]);
